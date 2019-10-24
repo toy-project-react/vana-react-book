630b24d44696e9fd2246240cc025a84508f1a1a7
vana <vanayun@naver.com>
2019-10-24 17:52:24 +0900

    styled component test

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Info from './Info';
 import SassComponent from './SassComponent';
 import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponent';
 
 function App() {
-  return <CSSModule />;
+  return <StyledComponent />;
 }
 
 export default App;
